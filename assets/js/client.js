d82ab0d562a1edb9528e64f98bdb5573b1914b8a
chanmi.kate.lee <chanmi.kate.lee@gmail.com>
2018-08-03 15:45:20 +0900

    Changing the condition of initializing Botkit

--- assets/js/client.js
+++ assets/js/client.js
@@ -479,5 +479,7 @@
     (function() {
       // your page initialization code here
       // the DOM will be available here
-      Botkit.boot();
+      if ( $(".embedded_messenger").length) {
+        Botkit.boot({});
+      }
     })();
